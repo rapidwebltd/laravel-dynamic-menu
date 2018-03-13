2d35a00f063705331d7b5f69ef1eccf5ae50bf76
Jordan Hall <jordan@hall05.co.uk>
2018-03-13 09:52:16 +0000

    Apply fixes from StyleCI

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RapidWeb\LaravelDynamicMenu\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -23,26 +24,25 @@ class Menu extends Model
 
     public function add($name, Menuable $menuable = null, $parent_id = 0)
     {
-        $menuItem = new MenuItem;
+        $menuItem = new MenuItem();
         $menuItem->menu_id = $this->id;
         $menuItem->parent_id = $parent_id;
         $menuItem->name = $name;
-        
+
         if ($menuable) {
             $menuItem->menuable_id = $menuable->id;
             $menuItem->menuable_type = get_class($menuable);
         }
 
         $menuItem->save();
-        
+
         return $menuItem;
     }
 
     public function clear()
     {
-        foreach($this->allMenuItems as $menuItem) {
+        foreach ($this->allMenuItems as $menuItem) {
             $menuItem->delete();
         }
     }
-
 }
